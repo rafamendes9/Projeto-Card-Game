64ad4147d213b106741284eff60e24562a843d07
V1t1nh0 <101417377+Victor0674@users.noreply.github.com>
2023-09-25 15:47:19 -0300

    Heritage

---
+++ C2_CartaShiny.java
@@ -0,0 +1,7 @@
+public class C2_CartaShiny extends C_Carta {
+    public C2_CartaShiny(String nome, String imagem, String tipo, J_Enum raridade,
+    String habilidade, int ataque, int defesa, int custo) {
+        super(nome, imagem, tipo, raridade, habilidade, ataque, defesa, custo);
+    }
+    
+}
---
+++ G2_ArenaDouble.java
@@ -0,0 +1,8 @@
+public class G2_ArenaDouble extends G_Arena{
+
+    public G2_ArenaDouble(A_Usuario jogador1, A_Usuario jogador2, D_Deck deckJogador1, D_Deck deckJogador2) {
+        super(jogador1, jogador2, deckJogador1, deckJogador2);
+    }
+
+    
+}
