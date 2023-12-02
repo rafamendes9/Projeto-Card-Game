757864f5eea3f1a4d9b5ceabffd5db074bd3b471
V1t1nh0 <101417377+Victor0674@users.noreply.github.com>
2023-12-02 00:28:05 -0300

    More stuff

--- CardGame/C4_InterfaceUserMenu.java
+++ CardGame/C4_InterfaceUserMenu.java
@@ -18,9 +18,10 @@ public class C4_InterfaceUserMenu {
 
         while (!sair) {
             //add funções de logar e dentro de logar, acessar a loja, jogar = entrar no lobby para jogar só ou em dupla e sair.
-            System.out.println("\n--- Menu de Criação de Usuários ---");
+            System.out.println("\n--- Menu de Inicial de Usuários ---");
             System.out.println("1. Criar novo usuário");
-            System.out.println("2. Sair");
+            System.out.println("2. Logar na conta");
+            System.out.println("3. Sair");
 
             System.out.print("Escolha uma opção: ");
             int escolha = scanner.nextInt();
@@ -31,6 +32,9 @@ public class C4_InterfaceUserMenu {
                     criarNovoUsuario(scanner);
                     break;
                 case 2:
+                    logar(scanner);
+                    break;
+                case 3:
                     sair = true;
                     break;
                 default:
@@ -68,6 +72,88 @@ public class C4_InterfaceUserMenu {
 
         // Chama o método para salvar no arquivo
         salvarUsuario(usuario);
+    }
+    // FIM DE CADASTRO
+
+    private static void logar(Scanner scanner){
+        String nomeTeste;
+        String senhaTeste;
+        boolean sair = false;
+
+        System.out.println("Digite seu nome:");
+        nomeTeste = scanner.nextLine();
+
+        System.out.println("Digite sua senha:");
+        senhaTeste = scanner.nextLine();
+
+        if () {
+            
+        } else {
+            
+        }
+        
+
+        while (!sair) {
+            System.out.println("\n--- Menu de Inicial do Lobby ---");
+            System.out.println("1. Entrar numa partida");
+            System.out.println("2. Entrar na Loja");
+            System.out.println("3. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int escolha = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
+
+            switch (escolha) {
+                case 1:
+                    entrarNumaPartida(scanner, usuario);
+                    break;
+                case 2:
+                    entrarNaLoja(scanner);
+                    break;
+                case 3:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+    }
+
+    private static void entrarNumaPartida(Scanner scanner, A_Usuario usuario){
+        boolean sair = false;
+
+        while (!sair) {
+            System.out.println("\n--- Menu de Inicial de Loja ---");
+            System.out.println("1. Entrar numa partida PVP");
+            System.out.println("2. Entrar numa partida 2V2");
+            System.out.println("3. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int escolha = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
+
+            switch (escolha) {
+                case 1:
+                    F_Lobby lobby = new F_Lobby();
+                    lobby.adicionarUsuario(usuario);
+                    break;
+                case 2:
+                    logar(scanner);
+                    break;
+                case 3:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+
+    }
+
+    private static void entrarNaLoja(Scanner scanner){
+
     }
 
 
